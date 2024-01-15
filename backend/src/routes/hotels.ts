67389ce204473cbf8a5ef1d1eb4e0cdebefc911f
ANHTRAN <tran36a@mtholyoke.edu>
2024-01-15 11:36:03 -0500

    show details backend

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -1,8 +1,29 @@
 import express, { Request, Response} from "express";
 import Hotel from "../models/hotel";
 import { HotelSearchResponse } from "../shared/types";
+import { param, validationResult } from "express-validator";
 
 const router = express.Router();
+
+router.get("/:id", [
+    param("id").notEmpty().withMessage("Hotel ID is required")
+], 
+async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array()})
+    }
+
+    const id = req.params.id.toString();
+    try {
+        const hotel = await Hotel.findById(id);
+        res.json(hotel)
+    } catch(e) {
+        console.log(e)
+        res.status(500).json({ message: "Error! Try again"})
+    }
+})
+
 router.get("/search", async (req: Request, res: Response) => {
     try {
         const query = constructSearchQuery(req.query);
