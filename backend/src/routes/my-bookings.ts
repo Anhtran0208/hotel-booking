c53b05e65c5c1954692e7a96ae84fc53e500cb83
ANHTRAN <tran36a@mtholyoke.edu>
2024-01-16 01:13:09 -0500

    view bookings backend

---
+++ backend/src/routes/my-bookings.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import verifyToken from "../middleware/auth";
+import {Request, Response} from "express"
+import Hotel from "../models/hotel";
+import { HotelType } from "../shared/types";
+
+const router = express.Router();
+router.get("/", verifyToken, async(req: Request, res: Response) => {
+    try{
+        const hotels = await Hotel.find({
+            bookings: {$elemMatch: {userId: req.userId}}
+        })
+
+        const results = hotels.map((hotel) => {
+            const userBookings = hotel.bookings.filter(
+                (booking) => booking.userId === req.userId
+            )
+
+            const hotelWithUserBookings: HotelType = {
+                ...hotel.toObject(),
+                bookings: userBookings,
+            }
+            return hotelWithUserBookings;
+        })
+        res.status(200).send(results)
+    } catch(e) {
+        console.log(e)
+        res.status(500).json({message: "Error viewing bookings"})
+    }
+})
+export default router;
