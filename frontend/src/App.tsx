54b0ce4f5d3376b00fa7f2e87f7ec268d08b4769
ANHTRAN <tran36a@mtholyoke.edu>
2024-01-15 22:18:18 -0500

    frontend booking details

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Details from "./pages/Details";
+import Booking from "./pages/Booking";
 
 const App = () => {
   const {isLoggedIn} = useAppContext();
@@ -40,6 +41,12 @@ const App = () => {
 
         {isLoggedIn && (
           <>
+          <Route path="/hotel/:hotelId/booking" element={
+            <Layout>
+              <Booking />
+            </Layout>
+          } />
+
           <Route path="/add-hotel" element={
             <Layout>
               <AddHotel />
--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -1,7 +1,44 @@
 import { useQuery } from "react-query"
 import * as apiClient from "../api-client"
+import BookingForm from "../forms/BookingForm/BookingForm"
+import { useSearchContext } from "../context/SearchContext"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import BookingDetailSummary from "../components/BookingDetailSummary"
 
 const Booking = () => {
+    const search = useSearchContext();
+    const {hotelId} = useParams();
+
+    const {data: hotel} = useQuery("fetchHotelById", 
+    () => apiClient.fetchHotelById(hotelId as string), 
+    {enabled: !!hotelId})
+
+    const [lengthOfStay, setLengthOfStay] = useState<number>(0)
+    useEffect(() => {
+        if (search.checkIn && search.checkOut){
+            const numberOfNights = Math.abs(search.checkOut.getTime() - search.checkIn.getTime()) / (1000*60*60*24);
+        setLengthOfStay(Math.ceil(numberOfNights))
+        }
+    }, [search.checkIn, search.checkOut])
+
     const {data: currentUser} = useQuery("viewCurrentUser", apiClient.viewCurrentUser)
+    if (!hotel){
+        return <></>
+    }
+    return (
+        <div className="grid md:grid-cols-[1fr_2fr]">
+            <BookingDetailSummary 
+            checkIn={search.checkIn}
+            checkOut={search.checkOut}
+            adultCount={search.adultCount}
+            childrenCount={search.childrenCount}
+            lengthOfStay={lengthOfStay}
+            hotel={hotel}
+            />
+            {currentUser && <BookingForm currentUser={currentUser} />}
+            
+        </div>
+    )
 }
 export default Booking;
